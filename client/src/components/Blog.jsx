a9488627cc1f2928975f65f43db205507785f0c4
tad@briq.com <tadchifamba@gmail.com>
2024-10-30 02:27:03 -0400

    small change to post title

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -31,7 +31,7 @@ const Blog = () => {
 
   return (
     <div className='container'>
-      <h1>Create Post!!!!!</h1>
+      <h1>Create Post</h1>
       <PostCreate updatePosts={updatePosts} />
       <div className="post-list">
       {
